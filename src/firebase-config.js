7a694f258e9d12ee16811122ec0a1baf388a4112
twitchprimeplease <melanieospitia@hotmail.com>
2023-05-25 20:59:19 -0500

    trabajo en la estructura de la base de datos con el shopping cart

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,14 @@
 
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc, doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 
 const firebaseConfig = {
-
+    apiKey: "AIzaSyBBNHs_UEKCTputSLoHZZXsL6wUjWPuJoA",
+    authDomain: "logitech-web-project.firebaseapp.com",
+    projectId: "logitech-web-project",
+    storageBucket: "logitech-web-project.appspot.com",
+    messagingSenderId: "99324536990",
+    appId: "1:99324536990:web:caee4958330bb57d6ab38f"
     };
 
 const app = initializeApp(firebaseConfig);
@@ -20,3 +25,11 @@ export async function getProducts(){
     return allProducts;
 }
 
+export async function setCart(username, product){
+    const userRef = doc(db, "shopping-cart", username);
+    await updateDoc(userRef, {
+        shoppingCart: arrayUnion(product),
+    });
+    // const docRef = await addDoc(collection(db, "shopping-cart"), product);
+    // console.log("Document written with ID: ", docRef.id);
+    }
