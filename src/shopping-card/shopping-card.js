97221c1c36d9d79fdaee3b3f80895ca935e2f60f
Melanie Santos ospitia <melaniesantosospitia@MacBook-Pro-de-Melanie.local>
2023-06-08 22:12:36 -0500

    funcion del carrito

--- src/shopping-card/shopping-card.js
+++ src/shopping-card/shopping-card.js
@@ -1,14 +1,25 @@
 import './styles.css'
 import { getCurrentUser, getCart } from '../firebase-config';
 
-// let shoppingCartContainer = querySelector('#shopping-card-container');
-// let priceContainer = querySelector('#price-container');
+let shoppingCartContainer = document.querySelector('#shopping-card-container');
 
+let currentUserUID
 let currentUser = getCurrentUser();
 if(currentUser =! 'local'){
-    let currentUserUID = currentUser.uid;
+    currentUserUID = currentUser.uid;
 }
 
+let carrito = getCart(currentUserUID);
+
+carrito.forEach(element => {
+
+    const cardObj = document.createElement('shopping-card-element');
+    cardObj.setAttribute('name', product.name);
+    cardObj.setAttribute('price', product.price);
+    cardObj.setAttribute('image', product.url[0]);
+    cardObj.setAttribute('type', product.type);
+    
+    shoppingCartContainer.append(cardObj);
+});
 
 
-// console.log(getCart(currentUserUID));
